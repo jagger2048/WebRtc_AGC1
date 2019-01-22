9e13631c0295e2e92b3e8a4a7a4a5228e711f8de
jagger <jagger2048@gmail.com>
2019-01-22 17:44:21 +0800

    Replace the AudioFile with dr_wav method for wav file read and write. Implement the C wraped WebRtc AGC algorithm.

--- webrtc_agc/WebrtcAGC.h
+++ webrtc_agc/WebrtcAGC.h
@@ -13,76 +13,67 @@ int my_WebrtcAGC(int16_t *data_in,int16_t *data_out,size_t kSamples,size_t sampl
 	int maxLevel = 255;
 	int agcMode = kAgcModeFixedDigital;
 	WebRtcAgc_Init(hAGC, minLevel, maxLevel, agcMode, sample_rate);
-
 	WebRtcAgcConfig agcConfig;
-	agcConfig.targetLevelDbfs = 12;			// 3 default
-	agcConfig.compressionGaindB = 20;		// 9 default
+	agcConfig.targetLevelDbfs = 9;			// 3 default
+	agcConfig.compressionGaindB = 15;		// 9 default
 	agcConfig.limiterEnable = 1;
 	WebRtcAgc_set_config(hAGC, agcConfig);
+
 	// initialize the splitting filter 
-	mSplittingFilter *splitting_filter_c;
+	mSplittingFilter *splitting_filter_c = NULL;
 	if (sample_rate > 16000)
 	{
 		splitting_filter_c = SplittingFilter_Create(sample_rate);
-
 	}
-
 	// 
 	int32_t outMicLevel = 0;
 	int32_t inMicLevel = 0;
 	int16_t echo = 0;
 	uint8_t saturationWarning = 0;
 
-	const size_t nFrames = sample_rate / 100;		// 160 320 480
+	const size_t nFrames = sample_rate / 100;		// 80 160 320 480
 
-	int16_t tmp_in[3][160]{};
-	int16_t tmp_out[3][160]{};
+
 	int16_t *data_in_band[3];
 	int16_t *data_out_band[3];
-
-	//data_in_band[0] = tmp_in[0];
-	//data_out_band[0] = tmp_out[0];
+	int16_t tmp_in[3][160]{};
+	int16_t tmp_out[3][160]{};
 	for (size_t kBands = 0; kBands < 3; kBands++)
 	{
-		data_in_band[kBands] = (int16_t*)calloc(160, sizeof(int16_t));
-		data_out_band[kBands] = (int16_t*)calloc(160, sizeof(int16_t));
+		data_in_band[kBands] = tmp_in[kBands];		
+		data_out_band[kBands] = tmp_out[kBands];
 	}
+	// or you can replace them like this
+	//for (size_t kBands = 0; kBands < 3; kBands++)
+	//{
+	//	data_in_band[kBands] = (int16_t*)calloc(160 , sizeof(int16_t)); 
+	//	data_out_band[kBands] = (int16_t*)calloc(160, sizeof(int16_t));
+	//}
 
 	for (size_t n = 0; n < kSamples / nFrames; n++)
 	{
 		if (nFrames == 160 || nFrames == 80)
 		{
-			// 16000 hz
-			memcpy(pCurIn,data_in_band[0],sizeof(int16_t)*nFrames);
+			// 16 000 hz and 8 000 hz
+			memcpy(data_in_band[0], pCurIn, nFrames * sizeof(int16_t));
 			int agcError = WebRtcAgc_Process(hAGC, data_in_band, 1, nFrames, data_out_band, inMicLevel, &outMicLevel, echo, &saturationWarning);
 			if (agcError != 0)
 			{
 				printf("Agc processes fail\n");
 				return 0;
 			}
-			memcpy(pCurOut, data_out_band, nFrames * sizeof(int16_t));
+			memcpy(pCurOut,data_out_band[0], nFrames * sizeof(int16_t));
 		}
 		else if (nFrames == 320)
 		{
-			int16_t tmp1[2][160]{};
-			int16_t tmp2[2][160]{};
-			int16_t tmp3[2][160]{};
-			int16_t *data_out_per_frame[2]{ tmp1[0],tmp1[1] };					// 2 = nBanks
-			int16_t *bands_data_in[2]{ tmp2[0],tmp2[1] };							// 2 = nBanks
-			int16_t *bands_data_out[2]{ tmp3[0],tmp3[1] };						// 2 = nBanks
-
-			//SplittingFilter_Analysis_s16(splitting_filter_c, pCurIn, &data_in_band[0]);
-			SplittingFilter_Analysis_s16(splitting_filter_c, pCurIn, bands_data_in);
-			//int agcError = WebRtcAgc_Process(hAGC, data_in_band, 2, 160, &data_out_band[0], inMicLevel, &outMicLevel, echo, &saturationWarning);
-			int agcError = WebRtcAgc_Process(hAGC, bands_data_in, 2, 160, bands_data_out, inMicLevel, &outMicLevel, echo, &saturationWarning);
+			SplittingFilter_Analysis_s16(splitting_filter_c, pCurIn, data_in_band);
+			int agcError = WebRtcAgc_Process(hAGC, data_in_band, 2, 160, data_out_band, inMicLevel, &outMicLevel, echo, &saturationWarning);
 			if (agcError != 0)
 			{
 				printf("Agc processes fail\n");
 				return 0;
 			}
-			SplittingFilter_Synthesis_s16(splitting_filter_c, bands_data_out, data_out_per_frame[0]);
-
-			//SplittingFilter_Synthesis_s16(splitting_filter_c, &data_out_band[0], pCurOut);
+			SplittingFilter_Synthesis_s16(splitting_filter_c, data_out_band, pCurOut);
 		}
 		else if (nFrames == 480)
 		{
@@ -96,18 +87,14 @@ int my_WebrtcAGC(int16_t *data_in,int16_t *data_out,size_t kSamples,size_t sampl
 			SplittingFilter_Synthesis_s16(splitting_filter_c, data_out_band, pCurOut);
 
 		}
-		pCurOut += n;
-		pCurIn += n;
+		pCurOut += nFrames;
+		pCurIn += nFrames;
 	}
 
-
-	for (size_t kBands = 0; kBands < 3; kBands++)
+	if (splitting_filter_c !=NULL)
 	{
-		free(data_in_band[kBands]);
-		free(data_out_band[kBands]);
+		SplittingFilter_Destory(splitting_filter_c);
 	}
-
-	printf(" %d %d", (kSamples / nFrames) * nFrames, kSamples);
 	return 0;
 }
 
--- webrtc_agc/my_splitting_filter_c.h
+++ webrtc_agc/my_splitting_filter_c.h
@@ -5,7 +5,6 @@
 
 #include "signal_processing_library.h"	// two_band_qmf
 #include "my_three_band_filter.h"		// three band filter
-//#include "audio_util.h"					// f32 <--> s16
 
 void f32_to_s16(const float* pIn, size_t sampleCount, int16_t* pOut )
 {
@@ -32,10 +31,10 @@ void s16_to_f32(const int16_t* pIn, size_t sampleCount, float* pOut)
 }
 
 typedef struct mSplittingFilter{
-	size_t sample_rate_;	// 32000,48000
+	size_t sample_rate_;							// 32000,48000
 	static const int kTwoBandStateSize = 6;
-	int two_band_analysis_state[2][kTwoBandStateSize];
-	int two_band_synthesis_state[2][kTwoBandStateSize];
+	int32_t two_band_analysis_state[2][kTwoBandStateSize];
+	int32_t two_band_synthesis_state[2][kTwoBandStateSize];
 
 	ThreeBandFilter* three_band_filter_48k;
 	float data_f32[480];
@@ -58,7 +57,6 @@ mSplittingFilter* SplittingFilter_Create(size_t sample_rate) {
 		else if (handles->sample_rate_ == 48000)
 		{
 			// Three band mode.
-
 			handles->three_band_filter_48k = ThreeBandFilter_Create(480);
 			if (handles->three_band_filter_48k == NULL)
 			{
@@ -70,8 +68,8 @@ mSplittingFilter* SplittingFilter_Create(size_t sample_rate) {
 			memset(handles->data_s16,0,sizeof(handles->data_s16));
 			for (size_t kBands = 0; kBands < 3; kBands++)
 			{
-				handles->three_band_f32[kBands] = (float*)calloc(160, sizeof(float));
-				handles->three_band_s16[kBands] = (int16_t*)calloc(160, sizeof(int16_t));
+				handles->three_band_f32[kBands] = (float*)malloc(160*sizeof(float));
+				handles->three_band_s16[kBands] = (int16_t*)malloc(160* sizeof(int16_t));
 			}
 		}
 		else
@@ -95,28 +93,13 @@ int SplittingFilter_Analysis_s16(mSplittingFilter *handles,const int16_t *data, 
 	}
 	else 
 	{
-		//float data_in_float[480]{};
-		//float bands_p[3][160] = {};
-		//float *data_out_float[3]{ bands_p[0],bands_p[1],bands_p[2] };	// a block of continuous memory is sugested 
-		//S16ToFloat(data, 480, data_in_float);
 
 		s16_to_f32(data, 480, handles->data_f32);
-
-		//float data_f32[480];
-		//int16_t data_s16[480];
-		//float *three_band_f32[3];
-		//int16_t *three_band_s16[3];
-
-		//ThreeBandFilter_Analysis(handles->three_band_filter_48k, data_in_float, 480, data_out_float);
 		ThreeBandFilter_Analysis(handles->three_band_filter_48k, handles->data_f32, 480, handles->three_band_f32);
-
 		for (size_t kBands = 0; kBands < 3; kBands++)
 		{
 			f32_to_s16(handles->three_band_f32[kBands], 160, bands[kBands]);
 		}
-		//FloatToS16(data_out_float[0], 160, bands[0]);
-		//FloatToS16(data_out_float[1], 160, bands[1]);
-		//FloatToS16(data_out_float[2], 160, bands[2]);
 	}
 	return 0;
 }
@@ -127,25 +110,15 @@ int SplittingFilter_Synthesis_s16(mSplittingFilter *handles, const int16_t* cons
 			bands[0], bands[1], 
 			160, 
 			data, 
-			handles->two_band_synthesis_state[0], handles->two_band_synthesis_state[0]);
+			handles->two_band_synthesis_state[0], handles->two_band_synthesis_state[1]);
 	}
 	else{
-		// it's better to use float format func instead of int16_t
-		//float tmp[3][160] = {};
-		//float *data_in_float[3]{ tmp[0],tmp[1],tmp[2] };
-		//float data_out_float[480]{};
-		//S16ToFloat(bands[0], 160, data_in_float[0]);	// |bands|'s memory may not be continous,it's better to 
-		//S16ToFloat(bands[1], 160, data_in_float[1]);	// convert it from int16_t into float band by band.
-		//S16ToFloat(bands[2], 160, data_in_float[2]);
-
 		for (size_t kBands = 0; kBands < 3; kBands++)
 		{
 			s16_to_f32(bands[kBands], 160, handles->three_band_f32[kBands]);
 		}
-		//ThreeBandFilter_Synthesis(handles->three_band_filter_48k,data_in_float, 160, data_out_float);
 		ThreeBandFilter_Synthesis(handles->three_band_filter_48k, handles->three_band_f32, 160, handles->data_f32);
 		f32_to_s16(handles->data_f32, 480, data);
-		//FloatToS16(data_out_float, 480, data);
 	}
 	return 0;
 }
@@ -162,6 +135,7 @@ int SplittingFilter_Destory(mSplittingFilter *handles) {
 					free(handles->three_band_f32[kBands]);
 					free(handles->three_band_s16[kBands]);
 				}
+				return 0;
 			}
 
 		}
